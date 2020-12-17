e31fa3f12be77664b9cb3d269dd18a5389f1a2b1
Jin-314 <yuujinyuujin77@gmail.com>
2020-12-18 00:02:41 +0900

    some chages

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from discord.ext import commands
+import discord
+import numpy as np
+
+BOT_PREFIX = ('??', '!!')
+
+client = commands.Bot(command_prefix=BOT_PREFIX)
+
+@client.command(description="あああああああああ",
+                brief="いいいあｓだｓｄ")
+async def add(*num: float):
+    await client.say(np.sum(num))
+
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+
+    #Botとメッセージの送信者が同じ場合は何もしない
+    if client.user == message.author:
+        return
+    if message.content.startswith("こんにちは"):
+        m = "こんにちは！" + message.author.name + "さん！\n"
+        await message.channel.send(m)
+
+@client.event
+async def on_ready():
+    print('以下のユーザー名でログインしています')
+    print('ユーザー名：' + client.user.name)
+    print('ユーザーid：' + str(client.user.id))
+    print('-------------------------------------')
+
+client.run(os.environ.get("DISCORD_TOKEN"))
